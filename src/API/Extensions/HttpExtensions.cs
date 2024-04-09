8d3698a96d69a632574346ed0cc45ef52e9d993d
Chhin Sras <75309663+therealsras@users.noreply.github.com>
2024-04-09 23:39:59 +0700

    Fix warning append headers

--- src/API/Extensions/HttpExtensions.cs
+++ src/API/Extensions/HttpExtensions.cs
@@ -5,7 +5,7 @@ public static class HttpExtensions
     {
         var options = new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
 
-        response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
-        response.Headers.Add("Pagination", JsonSerializer.Serialize(metaData, options));
+        response.Headers.Append("Access-Control-Expose-Headers", "Pagination");
+        response.Headers.Append("Pagination", JsonSerializer.Serialize(metaData, options));
     }
 }
